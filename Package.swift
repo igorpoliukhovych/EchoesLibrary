89b2c88eacd0f44ea329c83479d7ca99243ac0df
Igor Poliukhovych <igorpoliukhovychios@gmail.com>
2021-06-13 15:58:36 +0300

    Updated Realm target

--- Package.swift
+++ Package.swift
@@ -5,24 +5,21 @@ import PackageDescription
 
 let package = Package(
     name: "EchoesLibrary",
+    platforms: [
+        .iOS(.v11)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "EchoesLibrary",
-            targets: ["API"]),
+        .library(name: "API", targets: ["API"])
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-         .package(url: "https://github.com/realm/realm-cocoa.git", from: "10.0.0")
+        .package(name: "Realm", url: "https://github.com/realm/realm-cocoa.git", from: "10.0.0")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "API",
-            dependencies: []),
-        .testTarget(
-            name: "EchoesLibraryTests",
-            dependencies: ["API"]),
+        .target(name: "API", dependencies: [
+                    .product(name: "RealmSwift", package: "Realm")
+//                    "RealmSwift", "Realm", "RealmDatabase"
+        ]),
+        .testTarget(name: "EchoesLibraryTests", dependencies: ["API"])
     ]
 )
+
